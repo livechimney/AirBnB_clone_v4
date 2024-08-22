b98307327d0128ac204416fecb7d6854b979f697
Adrian Greg <adrianmuriuki1366@gmail.com>
2024-08-22 17:29:51 +0300

    bnbv4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """ Flask Application """
+import os
 from models import storage
 from api.v1.views import app_views
-from os import environ
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
+swagger = Swagger(app)
+app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1*": {"origins": "*"}})
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
 
 @app.teardown_appcontext
@@ -21,27 +26,58 @@ def close_db(error):
 
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(exception):
     """ 404 Error
     ---
     responses:
       404:
         description: a resource was not found
     """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    code = exception.__str__().split[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
 
-Swagger(app)
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+        Global Route to handle All Error Status Codes
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
 
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+    setup_global_errors()
+    host = os.environ.get('HBNB_API_HOST')
+    port = os.environ.get('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
